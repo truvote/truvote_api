2c521dd3b36aa602bce5e5d5af1ba86e2c0ce4a6
shaunlawrencecarland <thestarsareprojectors824@gmail.com>
2019-12-12 21:47:00 -0500

    add controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,10 @@ Rails.application.routes.draw do
   resources :representative_profiles
   resources :constituent_profiles
   resources :users
+
+  get 'login' => "sessions#new"
+  post 'login' => "sessions#create"
+  delete 'login' => "sessions#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
