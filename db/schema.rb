00c0ca535085d720e52b1b032fe53f71298a1d0f
shaunlawrencecarland <thestarsareprojectors824@gmail.com>
2019-12-12 21:57:49 -0500

    add password to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_13_022602) do
+ActiveRecord::Schema.define(version: 2019_12_13_025434) do
 
   create_table "bill_votes", force: :cascade do |t|
     t.integer "bill_id"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2019_12_13_022602) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
