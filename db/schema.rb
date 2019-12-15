b8b2010ff18bcb50f572d2414c73d79f4abd8823
shaunlawrencecarland <thestarsareprojectors824@gmail.com>
2019-12-15 03:02:25 -0500

    add role

--- db/schema.rb
+++ db/schema.rb
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2019_12_15_073910) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
+    t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
