6d43d51c12b80c091a99638f5743f161ee5d98cb
Shaun <shauncarland@gmail.com>
2020-01-17 07:20:14 -0500

    remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  resources :user_states
-  resources :bill_votes
-  resources :builds
-  resources :representative_profiles
-  resources :constituent_profiles
   resources :users
   resources :bills
   resources :votes
