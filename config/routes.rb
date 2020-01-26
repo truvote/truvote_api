a375dd40dfeb6cb50322f6c6a73c8fa0c3e0b97e
Shaun <shauncarland@gmail.com>
2020-01-26 15:22:42 -0500

    add bills page

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
+      resources :bills
     end
   end
-  
-  resources :bills
+
+  # resources :bills
   resources :votes
 
   get 'login' => "sessions#new"
