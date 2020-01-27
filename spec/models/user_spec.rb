7a960d04aa506e81465c6d2dc0da0a4e22104ce9
Shaun <shauncarland@gmail.com>
2020-01-27 09:09:07 -0500

    failing specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    it "foo" do
-        binding.pry
-        expect(1).to_not raise_error
+    describe "validations" do
+        describe "password" do
+            let(:user) { create(:user, password: password) }
+
+            context "when the password is six characters long" do
+                let(:password) { "a" }
+
+                it "raises an invalid password error" do
+                    # due to length of the password
+                    expect { user.save }.to raise_error
+                end
+            end
+
+            context "when the password is only composed of digits" do
+                let(:password) { "1234567" }
+
+                it "raises an invalid password error" do
+                    expect { user.save }.to raise_error
+                end
+            end
+
+            context "when the password is only composed of lowercase characters" do
+                let(:password) { "abcdefghijklmnop" }
+
+                it "raises an invalid password error" do
+                    expect { user.save }.to raise_error
+                end
+            end
+
+            context "when the password is only composed of upercase characters" do
+                let(:password) { "ABCDEFGHIJK" }
+
+                it "raises an invalid password error" do
+                    expect { user.save }.to raise_error
+                end
+            end
+        end
     end
 end
