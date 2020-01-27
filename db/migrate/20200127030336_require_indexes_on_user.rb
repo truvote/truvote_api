6813a16941f8ed50cdf52f53ffb053f194b14f02
Shaun <shauncarland@gmail.com>
2020-01-26 22:13:52 -0500

    Revert "run migrations and create migration"
    This reverts commit 69680485a68d198b7daf75b1d61dfd574106fc0b.

--- db/migrate/20200127030336_require_indexes_on_user.rb
+++
@@ -1,6 +0,0 @@
-class RequireIndexesOnUser < ActiveRecord::Migration[5.2]
-  def change
-    change_column :users, :email, :string, null: false
-    change_column :users, :password, :string, null: false
-  end
-end
