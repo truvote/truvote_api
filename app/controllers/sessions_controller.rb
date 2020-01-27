e95a0dd624d93cd41af5e10ef7ecb2156cb52e64
Shaun <shauncarland@gmail.com>
2020-01-27 08:51:40 -0500

    make sessions controller restful

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,17 +5,24 @@ class SessionsController < ApplicationController
 
   # POST /login
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(email: session_params[:email].downcase)
+    if user && user.authenticate(session_params[:password])
       log_in user
       head :created
     else
-      render json: { "Invalid email/password combination" }
+      render json: { errors: user.errors }
     end  
   end
 
   # DELETE /login
   def destroy
     log_out
+  end
+
+  private
+
+  # whitelist params from the scary internet
+  def session_params
+    params.require(:session).permit(:email,:password)
   end
 end
