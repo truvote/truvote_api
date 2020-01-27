bc0e7db135cb5fcefae895cd66d0520c3cb8af40
Shaun <shauncarland@gmail.com>
2020-01-26 21:54:26 -0500

    Fix user controller endpoints

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,30 +6,22 @@ module Api
       # GET /users
       # GET /users.json
       def index
-        @users = User.all
+        render json: User.all
       end
     
       # GET /users/1
       # GET /users/1.json
       def show
+        render json: @user
       end
-    
-      # GET /users/new
-      def new
-        @user = User.new
-      end
-    
-      # GET /users/1/edit
-      def edit
-      end
-    
+        
       # POST /users
       # POST /users.json
       def create
         @user = User.new(user_params)
 
         if @user.save
-          render json: { status: 201 }
+          render json: @user, status: :ok
         else
           render json: { status: 501 }
         end
@@ -38,24 +30,20 @@ module Api
       # PATCH/PUT /users/1
       # PATCH/PUT /users/1.json
       def update
-        respond_to do |format|
-          if @user.update(user_params)
-            format.html { redirect_to @user, notice: 'User was successfully updated.' }
-            format.json { render :show, status: :ok, location: @user }
-          else
-            format.html { render :edit }
-            format.json { render json: @user.errors, status: :unprocessable_entity }
-          end
+        if @user.update(user_params)
+          render json: @user, status: :ok
+        else
+          render json: { status: 501 }
         end
       end
     
       # DELETE /users/1
       # DELETE /users/1.json
       def destroy
-        @user.destroy
-        respond_to do |format|
-          format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-          format.json { head :no_content }
+        if @user.destroy
+          render json: { status: 200 }
+        else
+          render json: { status: 501 }
         end
       end
     
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController do
-    it "foo" do
-        binding.pry
+    describe "#create" do
+        let(:subject) { post :create, params: params }
+        let(:params) { { user:  { first_name: "Foo", last_name: "Bar", email: "foo@bar.com", password: "123abc" } } }
+
+        it "returns a 200 response" do
+            subject
+            expect(response.status).to eq(200)
+        end
+
+        it "creates a new user with the params" do
+            subject
+            user = User.last
+
+            expect(user.first_name).to eq("Foo")
+            expect(user.last_name).to eq("Bar")
+            expect(user.email).to eq("foo@bar.com")
+        end
     end
+
+    # todo: add #update specs
 end
